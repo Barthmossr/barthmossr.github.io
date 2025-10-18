15a6d2bdc613410664c5b4f0fe3b34e68869db8c
Barthmossr <barthmossr@gmail.com>
2025-10-18 17:57:15 -0300

    style: center content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import { TextGradient } from '@/components/TextGradient'
 
 export default function Home() {
   return (
-    <div>
-      <TextGradient as='h1' className='text-5xl font-bold'>
-        B
+    <div className='flex justify-center items-center h-screen w-screen overflow-hidden scroll-smooth'>
+      <TextGradient as='h1' className='text-8xl font-bold md:text-9xl'>
+        <button type='button' className='cursor-pointer'>
+          B
+        </button>
       </TextGradient>
     </div>
   )
