be60879f20131cfc1ad10946dc5e9f1cf783ca53
Barthmossr <barthmossr@gmail.com>
2025-10-18 11:21:08 -0300

    chore: add vitest and basic setup

---
+++ tests/unit/app/page.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/app/page'
+
+describe('Home', () => {
+  it('should render a h1 with the text B', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', { level: 1 })
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveTextContent('B')
+  })
+})
