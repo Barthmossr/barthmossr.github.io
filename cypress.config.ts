4fd7cf33fb8389b8053f1eff71c1b00b16df95af
Barthmossr <barthmossr@gmail.com>
2025-10-18 11:45:28 -0300

    chore: add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    supportFile: 'tests/e2e/support/e2e.ts',
+    specPattern: 'tests/e2e/**/*.cy.ts',
+    videosFolder: 'tests/e2e/videos',
+    screenshotsFolder: 'tests/e2e/screenshots',
+    fixturesFolder: 'tests/e2e/fixtures',
+  },
+})
